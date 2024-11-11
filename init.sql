9b9d385d43e521584cbd0a7c41966e75a5d6f4d1
FabioSimones <138723986+FabioSimones@users.noreply.github.com>
2024-11-11 17:27:10 -0300

    Update init.sql
    Signed-off-by: FabioSimones <138723986+FabioSimones@users.noreply.github.com>

--- init.sql
+++ init.sql
@@ -1,5 +1,17 @@
 USE test_db;
 
 --TODO Crie a tabela de user;
-
+Create table User(
+    ID_User int primary key AUTOINCREMENT,
+    FirstName varchar(100) not null,
+    LastName varchar(100) not null,
+    Email varchar(100) not null,
+    )
 --TODO Crie a tabela de posts;
+Create table Post(
+    ID_Post int primary key AUTOINCREMENT,
+    Title varchar(100) not null,
+    Description varchar(100) not null,
+    ID_User int,
+    constraint fk_UserPost FOREIGN KEY (ID_User) REFERENCES User (ID_User)
+    )
